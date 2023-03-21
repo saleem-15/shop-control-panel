a8905bc5112fe6e6d9a2ec215707b21267e57676
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-03-21 15:34:53 +0200

    Refactor: Assets are orgnized

---
+++ lib/utils/constants/assets.dart
@@ -0,0 +1,13 @@
+class Assets {
+  Assets._();
+
+  /// illustrations
+  static const LOGIN_ILLISTRUTION = 'assets/illustrations/sign-up-form.svg';
+  static const LOGIN_ILLISTRUTION2 = 'assets/illustrations/otp-security.svg';
+  static const LOGIN_ILLISTRUTION3 = 'assets/illustrations/palm-recognition.svg';
+
+  /// icons
+  static const IC_LOCK = 'assets/icons/lock_icon_32.png';
+  static const IC_SHOW = 'assets/icons/show_icon_32.png';
+  static const IC_HIDE = 'assets/icons/hide_icon_32.png';
+}
